b7235a7d01e786d101dc38ea640bfc8af3915cdd
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-05-20 22:46:14 +0500

    Добавлен requirements, сбор данных в файл

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils.translation.trans_real import catalog
 
 
 def home(request):
@@ -6,6 +7,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        with open('datas.txt', "w") as file:
+            file.write(f'{name}, {phone}, {message}')
     return render(request, 'contacts.html')
 
-# Create your views here.
