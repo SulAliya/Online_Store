1ea00469778723fb3a488f47811ee3a0993eb6d3
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-05-26 12:54:19 +0500

    Созданы модели Product, Category

--- config/settings.py
+++ config/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store',
+        'USER': 'postgres',
+        'PASSWORD': 'Qwerty098',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
     }
 }
 
@@ -96,3 +100,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
