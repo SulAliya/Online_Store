8c684d907794c63fdd2ce00a7696945d7da8f00c
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-06-13 22:41:51 +0500

    Новая модель Blog

---
+++ blog/views.py
@@ -0,0 +1,54 @@
+
+from django.urls import reverse_lazy, reverse
+from django.utils.text import slugify
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from blog.models import Blog
+
+
+class BlogListView(ListView):
+    model = Blog
+    fields = ('title', 'body', 'preview', 'created_at', 'publication_sing')
+    success_url = reverse_lazy('blog:blog_list')
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(publication_sing=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_of_views += 1
+        self.object.save()
+        return self.object
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'body', 'preview', 'created_at', 'publication_sing')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'body', 'preview', 'created_at', 'publication_sing')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', kwargs={'pk': self.object.pk})
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:blog_list')
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ContactsTemplateView, ProductDetailView, ProductCreateView, \
-    ProductUpdateView, ProductDeleteView
+from catalog.views import ProductListView, ContactsTemplateView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_catalog'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('catalog/create', ProductCreateView.as_view(), name='product_create'),
-    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts')
+
 
 ]
